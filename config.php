64de1d4ba77c36b2404f03395d269214c5bd5865
JosueMiller <164652572+JosueMiller@users.noreply.github.com>
2026-02-14 12:30:45 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-$host = "proyecto-demo-cloudsql-487413:us-east1:mi73710420";
+$host = "localhost";
 $dbname = "db_cloud";
 $username = "usermysql";
 $password = "Jm2026*+";
@@ -20,6 +20,7 @@ try {
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
+
 
 
 
